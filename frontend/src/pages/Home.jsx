771e73aee8556ff40da30fbba00c114e045f5c75
Nmait2111 <namitjain2111@gmail.com>
2024-09-22 19:51:21 +0530

    basic layout for frontend and backend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h1>Coming Soon...</h1>
+      <style jsx>{`
+        .home-page {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          font-size: 30px;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default Home
